f52d1caef955c96cde5313ebce76af41eb651315
daveuhlik <daveuhlik@users.noreply.github.com>
2020-11-13 18:38:59 +0100

    Added js nav

---
+++ navbar.js
@@ -0,0 +1,6 @@
+let mainNav = getElementById('js-menu');
+let navToggle = getElementById('js-navbar-toggle')
+
+navToggle.addEventListener('click', function() {
+	mainNav.classList.toggle('active')
+})
