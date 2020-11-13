a6da6d2a8403205b94c7ace63f7231e6fb65e339
daveuhlik <daveuhlik@users.noreply.github.com>
2020-11-13 20:34:32 +0100

    Fix toggle

--- navbar.js
+++ navbar.js
@@ -2,5 +2,5 @@ let mainNav = getElementById('js-menu');
 let navToggle = getElementById('js-navbar-toggle')
 
 navToggle.addEventListener('click', function() {
-	mainNav.classList.toggle('active')
+	mainNav.classList.toggle('active');
 })
